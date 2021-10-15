6b8895546fd538ab18edd86a80b8d42f9541b02f
unknown <francovivas14@gmail.com>
2021-10-15 11:58:48 -0300

    Parcial 1

---
+++ Primer parcial/src/perros.h
@@ -0,0 +1,18 @@
+#ifndef PERROS_H_
+#define PERROS_H_
+#define TAMCHAR 21
+typedef struct
+{
+	int id;
+	char nombre[TAMCHAR];
+	char raza[TAMCHAR];
+	int edad;
+	int vacio;
+}ePerro;
+
+int perro_buscarPerroCargado(ePerro* perro,int tam,int* indice,int idPerro);
+int perro_ListarPerros(ePerro* perro,int tam);
+void perro_ListarPerro(ePerro perro);
+int perro_ListarPromedioEdadPerros(ePerro* perro,int tam);
+
+#endif /* PERROS_H_ */
